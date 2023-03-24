5192e812b894c458b4aaac46c3f82f4a845bada9
Shaylin Simadari <ssi162@uclive.ac.nz>
2023-03-25 02:20:42 +1300

    UHHH

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Tree from 'react-tree-graph';
+import tasks from './tasks.json'
 
 function App() {
   const [count, setCount] = useState(0)
+  let data = {
+    name: 'Parent',
+    children: [{
+      name: 'Child One'
+    }, {
+      name: 'Child Two'
+    }]
+  };
+
+  const x = () => {
+    let s = ""
+    for(const [k, v] of Object.entries(tasks)){
+      s += k
+    }
+    return s
+  }
 
   return (
     <div className="App">
-      POO
+    {/* <Tree
+      data={data}
+      height={400}
+      width={400}
+    /> */}
+      {/* {JSON.stringify(Object.entries(tasks))} */}
+      {x()}
     </div>
   )
 }
